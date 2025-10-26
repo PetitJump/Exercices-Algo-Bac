9a38c65b0c24fb55106f4b7b3acf06ba9628dbfb
PetitJump <killianbonnin7@gmail.com>
2025-10-26 12:45:30 +0100

    Ajout de plusieur algo, modification du README. Projet en phase de test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Des petits exercices pour s'améliorer en algo
 A terme il y aura des exos des anals du bac
+Le but est de le faire sans s'aider de l'IA. On peut l'utiliser si on est vraiment bloquer pour qu'il nous donne des indices. Mais si vous êtes là c'est que vous voulez apprendre. L'IA vous aidera a coder plus vite (et encore c'est rare) mais il va vous voler votre apprentissage. Jouer le jeu
--- lvl1.py
+++ lvl1.py
@@ -1,3 +1,4 @@
+############################################################################################
 
 def plus_gand(tabl: list[int]) -> int:
     """Renvoie la valeur la plus grande d'un tableau"""
@@ -10,6 +11,43 @@ def plus_gand(tabl: list[int]) -> int:
 assert plus_gand([1, 2, 7, 3, 4, 5]) == 7 #La fonction plusgrand() doit renvoyer 7 car c'est la valeur la plus grande dans [1, 2, 7, 3, 4, 5]
 print("Fonction plus_grand() fonctionnel !")
 
+############################################################################################
 
-def inverser(tabl: list):
+def inverser(tabl: list) -> list:
     """Inverse une liste"""
+    rendu = []
+    for i in range(len(tabl), ..., -1): #On commence a i=len(tabl) puis i-=1 à chaque boucle ; on s'arretera ensuite lorsque i=0. On utilise ici le range(start, stop, step)
+        rendu.append(tabl[...]) #On ajoute à rendu la valeur que l'on traite actuelement
+    return ...
+
+assert inverser([1, 2, 3]) == [3, 2, 1] 
+print("Fonction inverser() fonctionnel !")
+
+############################################################################################
+
+def rechercher(tabl: list, x) -> int:
+    """Recherche x dans le tableau et renvoie la valeur où il y était dans le tableu"""
+    for i in range(...):
+        if ... == x:
+            return ...
+
+assert rechercher([1, 2, 34, "a", "S.Meden", 324, 4], 4) == 6
+assert rechercher([1, 2, 34, "a", "S.Meden", 324, 4], "a") == 3
+print("Fonction rechercher() fonctionnel !")
+
+############################################################################################
+
+def repetition(texte: str) -> dict:
+    """Compte de nombre de répétition de lettre dans un texte"""
+    rendu = {}
+    for i in range(len(texte)):
+        if texte[i] in rendu:
+            rendu[texte[i]] += 1
+        else:
+            rendu[texte[i]] = 1
+    return rendu
+
+assert repetition("test") == {"t" : 2, "e" : 1, "s" : 1}
+print("Fonction repetition() fonctionnel !")
+
+############################################################################################
