80093bce1bdc6e3da7942d40b6881427ec63b561
PetitJump <killianbonnin7@gmail.com>
2025-10-29 18:49:04 +0100

    Ajout des niveau dans lvl1.py dans Pile

--- Pile/lvl1.py
+++ Pile/lvl1.py
@@ -1,3 +1,4 @@
+########################### Class Pile à connaitre ##############################
 class Pile:
     def __init__(self):
         """Crée une pile vide"""
@@ -26,3 +27,42 @@ class Pile:
     def __repr__(self):
         """Affiche le contenu de la pile"""
         return str(self.__data)
+
+##################################### Exercices ################################
+
+
+def exo1():
+    """But : finir avec p = [1, 2, 3]"""
+    p = Pile()
+    p.empiler(4)
+    p.empiler(7)
+    p.empiler(2)
+    for i in range(3): #On commence par dépiler toute la Pile
+        ...
+    for i in range(3): #Puis on réenpile de 1 à 3
+        ...
+    print(p) #Il faut que lorsque on print p nous voyons [1, 2, 3]
+
+print("Exo 1 :")
+exo1()
+print("Si vous voyez juste au dessus [1, 2, 3] alors tout va bien")
+print("")
+
+############################################################################################
+
+
+def vider_pile(p: Pile):
+    """Vide complètement la Pile p"""
+    while ...: #Tant qu'il reste des éléments dans p
+        print(f"On retire : {...}")
+
+p = Pile()
+p.empiler(4)
+p.empiler(7)
+p.empiler(2)
+print("Exo vider_pile :")
+vider_pile(p)
+assert p.est_vide() == True
+print("Fonction vider_pile fonctionnel !")
+
+############################################################################################
