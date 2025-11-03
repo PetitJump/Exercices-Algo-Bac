a49513e076ae32dc8846e0431dc93e748cac6a2a
PetitJump <killianbonnin7@gmail.com>
2025-11-03 18:45:32 +0100

    Ajout d'un exercice expert dans List

--- List/expert.py
+++ List/expert.py
@@ -1,1 +1,7 @@
-#En cours de construction
+
+def contient(list: list, motif: list) -> bool:
+    """Regarde et detecte si un motif (sous-liste) apparait dans une liste"""
+    ...
+
+assert contient([1, 2, 3, 4, 5], [3, 4]) == True
+assert contient([1, 2, 3, 4, 5], [4, 2]) == False
