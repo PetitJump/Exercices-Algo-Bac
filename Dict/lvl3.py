20b08557230da96a867b3f5c37c6c68f916b266a
PetitJump <killianbonnin7@gmail.com>
2025-11-03 16:49:06 +0100

    Modification du lvl3.py dans le fichier Dict

--- Dict/lvl3.py
+++ Dict/lvl3.py
@@ -3,15 +3,17 @@
 def notes_eleves(donnees: list[list]) -> dict:
     """Prend des données et renvoie un dictionnaire pour faire la moyenne des notes"""
 
-    result = {} #Dictionnaire vide
+    result = ... #Dictionnaire vide
     for i in range(len(donnees)):
-        if donnees[i][0] not in result:
-            result[donnees[i][0]] = {"nb_notes": 1, "moyenne": donnees[i][1]}
+        nom = donnees[i][0]
+        note = donnees[i][1]
+        if ...: #Si le nom n'est pas dans result
+            result[...] = {"nb_notes": 1, "moyenne": note} #On initialise (aider vous de l'assert si besoin)
         else:
-            ancien_total = result[donnees[i][0]]["moyenne"] * result[donnees[i][0]]["nb_notes"]
-            result[donnees[i][0]]["nb_notes"] += 1
-            result[donnees[i][0]]["moyenne"] = (ancien_total + donnees[i][1]) / result[donnees[i][0]]["nb_notes"]
-    return result
+            ancien_total = ... * ... #On multiplie la moyenne actuel et le nombre de note
+            ... += 1 #On ajoute +1 aux nombres de notes
+            result[...]["moyenne"] = (... + ...) / ... #On calcule la moyenne (on utilise 'ancien_total' quelque part ;)
+    return ...
 
 assert notes_eleves([("Alice", 10), ("Bob", 15), ("Alice", 20)]) == {
     "Alice": {"nb_notes": 2, "moyenne": 15.0},
